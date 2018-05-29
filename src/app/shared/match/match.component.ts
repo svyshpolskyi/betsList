8d9dd927943c8bb862a1b87f7ff492720edd032c
Serhii Vyshpolskyi <svyshpol@softserveinc.com>
2018-05-29 12:56:31 +0300

    fix

--- src/app/shared/match/match.component.ts
+++ src/app/shared/match/match.component.ts
@@ -1,17 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-match',
   templateUrl: './match.component.html',
   styleUrls: ['./match.component.scss']
 })
-export class MatchComponent implements OnInit {
+export class MatchComponent {
 
   @Input () match;
-
-  ngOnInit() {
-    console.log(this.match);
-  }
-
 
 }
